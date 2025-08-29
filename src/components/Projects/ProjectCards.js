b5f1d5e510eb187cb3c732fc7af6ec8053b4d7aa
Mahi_09 <darlingmahee09@gmail.com>
2025-08-29 10:09:22 +0530

    commit

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -27,7 +27,7 @@ function ProjectCards(props) {
             variant="primary"
             href={props.demoLink}
             target="_blank"
-            style={{ marginLeft: "10px" }}
+            style={{ marginLeft: "40px" }}
           >
             <CgWebsite /> &nbsp;
             {"Demo"}
